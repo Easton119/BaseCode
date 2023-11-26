c492a61f8d7a929a59b274b123ff6d8f7422a398
qyj <1137109222@qq.com>
2023-11-26 19:02:28 +0800

    add : 文件io函数

--- test/test.cpp
+++ test/test.cpp
@@ -1,10 +1,24 @@
 #include<iostream>
 #include<memory>
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
 using namespace std;
 int main(){
-   cout<<"hello c++"<<endl;
-   unique_ptr<int> p=make_unique<int>(200);
-   unique_ptr<int> uptr2 = move(p);
-   system("pause");
+   // cout<<"hello c++"<<endl;
+   int fd = open("test.txt",O_CREAT|O_RDWR,0777);
+   int fd2 = open("test1.txt",O_CREAT|O_RDWR,0777);
+   cout<<fd<<endl;
+   int s = lseek(fd,0,SEEK_END);
+   cout<<s<<endl;
+
+   char buf[] = "hello world2333";
+   int len = 100;
    
+   write(fd2,buf,sizeof(buf));
+   close(fd);
+   close(fd2);
 }
